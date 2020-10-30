a4967ea2df7677598008943f62902885f53d6184
jdec92 <juande_estrella@uma.es>
2020-10-30 18:32:43 +0100

    Refactor code Motor

--- Motor/Motor.cpp
+++ Motor/Motor.cpp
@@ -6,41 +6,31 @@
 
 #include "Motor.h"
 
-Motor::Motor(uint8_t pin){
-	this->pin = pin;
+Motor::Motor(uint8_t pin) {
+    this->pin = pin;
 }
 
-void Motor::initialize(){
-	servo.attach(pin, pulseMinMotor, pulseMaxMotor);
+void Motor::initialize() {
+    servo.attach(pin, pulseMinMotor, pulseMaxMotor);
 }
 
-void Motor::updateValue(float value){
-	 servo.writeMicroseconds(value);
+void Motor::updateValue(float value) {  //valor en microsegundos
+    servo.writeMicroseconds(value);
 }
 
-void Motor::controller(float throttle,float pidPitch,float pidRoll,float pidYaw){
-	if(throttle < 1060){
-		updateValue(throttle);
-	}else{
-		Serial.println("+++++++++++++Motor++++++++++++");
-		Serial.print(throttle);
-		Serial.print("\t");
-		Serial.print(pidPitch);
-		Serial.print("\t");
-		Serial.print(pidRoll);
-		Serial.print("\t");
-		Serial.println(pidYaw);
-		Serial.println("+++++++++++++Motor++++++++++++");
-
-		float pulso = throttle + pidPitch + pidRoll + pidYaw;
-		if(pulso < 1030){ //Evitamos que en pleno vuelo se pare por completo el motor
-			pulso = 1025;
-		}
-		if(pulso > pulseMaxMotor){  //Evitamos meter valores superiores al motor
-			pulso = pulseMaxMotor;
-		}
-		updateValue(pulso);
-	}
+void Motor::controller(float throttle, float pitch, float roll, float yaw) {
+    if (throttle < 1060) {  //valor throttle minimo antes de despegar con lo cual no necesita control estabilidad
+        updateValue(throttle);
+    } else {    // activamos el control de estabilidad
+        float pulso = throttle + pitch + roll + yaw;
+        if (pulso < 1030) { //Evitamos que en pleno vuelo se pare por completo el motor
+            pulso = 1025;
+        }
+        if (pulso > pulseMaxMotor) {  //Evitamos meter valores superiores al pulso maximo de motor
+            pulso = pulseMaxMotor;
+        }
+        updateValue(pulso);
+    }
 }
 
 
